c0ae21b7001b52814366b6978cbb29684bf586c4
School computer 10 <itayanima@gmail.com>
2022-12-04 13:59:29 +0200

    completed getting which contacts have a user

--- app/src/main/java/com/itaypoo/photoblocks/ChooseContactActivity.kt
+++ app/src/main/java/com/itaypoo/photoblocks/ChooseContactActivity.kt
@@ -3,16 +3,14 @@ package com.itaypoo.photoblocks
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.firebase.firestore.AggregateSource
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.itaypoo.helpers.ContactModel
 import com.itaypoo.helpers.ContactsUtils
 import com.itaypoo.photoblocks.databinding.ActivityChooseContactBinding
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 class ChooseContactActivity : AppCompatActivity() {
     private lateinit var binding: ActivityChooseContactBinding
@@ -30,20 +28,39 @@ class ChooseContactActivity : AppCompatActivity() {
         database = Firebase.firestore
 
         contactsList = ContactsUtils.getList(this.contentResolver)
-        contactsThatHaveUser = getContactsThatHaveUser(contactsList)
+        //contactsThatHaveUser = getContactsThatHaveUser(contactsList)
 
+        GlobalScope.launch(Dispatchers.IO) {
+            for(c in contactsList){
+                val res = async { contactHasUser(c) }
+                Log.d("This", "Name: ${c.displayName}    , Count: ${res.await()}")
+                while(res == null){}
+            }
 
+        }
+        Log.d("This", "After")
     }
 
-    private fun getContactsThatHaveUser(contactsList: MutableList<ContactModel>): MutableList<String> {
-        val res = mutableListOf<String>()
+    suspend fun contactHasUser(contact: ContactModel): Int {
+        var count = 0
 
-        for(contact in contactsList){
-            // Loop through all contacts and separate the ones that have a user in the database
-            database.collection("users").whereEqualTo("phoneNumber", contact.phoneNumber).get()
+        var complete = false
+
+        // Start a query of counting the users with this contacts phone number
+        val countQuery = database.collection("users").whereEqualTo("phoneNumber", contact.phoneNumber).count().get(AggregateSource.SERVER)
+        countQuery.addOnSuccessListener {
+            count = it.count.toInt()
+        }.addOnCompleteListener{
+            complete = true
         }
 
-        return res
+        // Wait until query is complete
+        while (!complete) {
+            delay(1)
+        }
+
+
+        return count
     }
 
 }
